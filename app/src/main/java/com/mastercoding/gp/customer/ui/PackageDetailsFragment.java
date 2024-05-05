e0d0effa236d1f46a97955fbafd76739e1f45018
NourAldeen001 <118866955+NourAldeen001@users.noreply.github.com>
2024-05-05 18:43:02 +0300

    Create PackageDetailsFragment.java, make fragment_package_details.xml ui and add in nav_customer_graph.xml

---
+++ app/src/main/java/com/mastercoding/gp/customer/ui/PackageDetailsFragment.java
@@ -0,0 +1,28 @@
+package com.mastercoding.gp.customer.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mastercoding.gp.R;
+
+
+public class PackageDetailsFragment extends Fragment {
+
+
+
+    public PackageDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_package_details, container, false);
+    }
+}
