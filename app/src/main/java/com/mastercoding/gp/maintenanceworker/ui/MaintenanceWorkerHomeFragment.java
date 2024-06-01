4def4f33a12d7e925b06b15ac999158c7c9c466c
NourAldeen001 <118866955+NourAldeen001@users.noreply.github.com>
2024-06-01 19:24:22 +0300

    Add TextRecognizer MLKit in FirebaseML to Dependencies. Add Functionality of TextRecognizer in CleaningWorkerHomeFragment.java, MaintenanceWorkerHomeFragment.java, ParkingWorkerHomeFragment.java. Access Camera Within AndroidManifest.xml.

--- app/src/main/java/com/mastercoding/gp/maintenanceworker/ui/MaintenanceWorkerHomeFragment.java
+++ app/src/main/java/com/mastercoding/gp/maintenanceworker/ui/MaintenanceWorkerHomeFragment.java
@@ -1,22 +1,42 @@
 package com.mastercoding.gp.maintenanceworker.ui;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 import com.mastercoding.gp.CustomDialogFragment;
 import com.mastercoding.gp.R;
 import com.mastercoding.gp.SessionSharedPreferences;
 import com.mastercoding.gp.databinding.FragmentMaintenanceWorkerHomeBinding;
 import com.mastercoding.gp.parkingworker.data.ParkingWorkerFinishTaskBody;
 import com.mastercoding.gp.parkingworker.ui.viewmodel.ParkingWorkerFinishTaskViewModel;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class MaintenanceWorkerHomeFragment extends Fragment {
@@ -30,6 +50,14 @@ public class MaintenanceWorkerHomeFragment extends Fragment {
     CustomDialogFragment dialogFragment;
 
     String userName, password, base, authHeader;
+
+    private Bitmap imageBitmap;
+
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+
+    private ActivityResultLauncher<String> requestPermissionLauncher;
+
+    private final Set<String> countriesToFilter = new HashSet<>(Arrays.asList("EGYPT", "USA", "KSA"));
 
     public MaintenanceWorkerHomeFragment() {
         // Required empty public constructor
@@ -72,6 +100,87 @@ public class MaintenanceWorkerHomeFragment extends Fragment {
             }
         });
 
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == requireActivity().RESULT_OK) {
+                            Intent data = result.getData();
+                            assert data != null;
+                            Bundle extras = data.getExtras();
+                            imageBitmap = (Bitmap) extras.get("data");
+                            detectText();
+                        }
+                    }
+                });
+
+        requestPermissionLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestPermission(),
+                isGranted -> {
+                    if (isGranted) {
+                        Toast.makeText(requireContext(), "Permissions Granted..", Toast.LENGTH_SHORT).show();
+                        captureImage();
+                    } else {
+                        Toast.makeText(requireContext(), "Permissions Denied..", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        binding.maintenanceWorkerHomeCameraBtn.setOnClickListener(view -> {
+            if (checkPermissions()) {
+                captureImage();
+            } else {
+                requestPermission();
+            }
+        });
+
         return binding.getRoot();
+    }
+
+    private boolean checkPermissions() {
+        int cameraPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA);
+        return cameraPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+    }
+
+
+    private void captureImage() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(requireActivity().getPackageManager()) != null) {
+            someActivityResultLauncher.launch(takePicture);
+        }
+    }
+
+    private void detectText() {
+        if (imageBitmap == null) {
+            Toast.makeText(requireContext(), "Please capture an image first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        InputImage image = InputImage.fromBitmap(imageBitmap, 0);
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        recognizer.process(image)
+            .addOnSuccessListener(text -> {
+                StringBuilder result = new StringBuilder();
+                for(Text.TextBlock block: text.getTextBlocks()){
+                    String blockText = block.getText();
+                    result.append(filterCountries(blockText));
+                }
+                binding.maintenanceWorkerHomeCarPlateEditTxt.setText(result);
+            })
+            .addOnFailureListener(e -> Toast.makeText(requireContext(), "Failed to detect text from image: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+    private String filterCountries(String text) {
+        String[] words = text.split("\\s+");
+        StringBuilder filteredText = new StringBuilder();
+        for (String word : words) {
+            if (!countriesToFilter.contains(word)) {
+                filteredText.append(word);
+            }
+        }
+        return filteredText.toString().trim();
     }
 }
--- app/src/main/java/com/mastercoding/gp/parkingworker/ui/ParkingWorkerHomeFragment.java
+++ app/src/main/java/com/mastercoding/gp/parkingworker/ui/ParkingWorkerHomeFragment.java
@@ -1,20 +1,36 @@
 package com.mastercoding.gp.parkingworker.ui;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 import com.mastercoding.gp.CustomDialogFragment;
 import com.mastercoding.gp.R;
 import com.mastercoding.gp.SessionSharedPreferences;
@@ -28,6 +44,10 @@ import com.mastercoding.gp.parkingworker.ui.viewmodel.ParkingWorkerFinishTaskVie
 import com.mastercoding.gp.parkingworker.ui.viewmodel.ParkingWorkerGetCapacityOfBranchViewModel;
 import com.mastercoding.gp.parkingworker.ui.viewmodel.ParkingWorkerGetCountVisitationOfBranchViewModel;
 import com.mastercoding.gp.parkingworker.ui.viewmodel.ParkingWorkerRecordViewModel;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class ParkingWorkerHomeFragment extends Fragment {
@@ -47,6 +67,14 @@ public class ParkingWorkerHomeFragment extends Fragment {
     CustomDialogFragment dialogFragment;
 
     String userName, password, base, authHeader;
+
+    private Bitmap imageBitmap;
+
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+
+    private ActivityResultLauncher<String> requestPermissionLauncher;
+
+    private final Set<String> countriesToFilter = new HashSet<>(Arrays.asList("EGYPT", "USA", "KSA"));
 
     public ParkingWorkerHomeFragment() {
         // Required empty public constructor
@@ -137,7 +165,88 @@ public class ParkingWorkerHomeFragment extends Fragment {
             }
         });
 
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == requireActivity().RESULT_OK) {
+                            Intent data = result.getData();
+                            assert data != null;
+                            Bundle extras = data.getExtras();
+                            imageBitmap = (Bitmap) extras.get("data");
+                            detectText();
+                        }
+                    }
+                });
+
+        requestPermissionLauncher = registerForActivityResult(
+                new ActivityResultContracts.RequestPermission(),
+                isGranted -> {
+                    if (isGranted) {
+                        Toast.makeText(requireContext(), "Permissions Granted..", Toast.LENGTH_SHORT).show();
+                        captureImage();
+                    } else {
+                        Toast.makeText(requireContext(), "Permissions Denied..", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        binding.parkingWorkerHomeCameraBtn.setOnClickListener(view -> {
+            if (checkPermissions()) {
+                captureImage();
+            } else {
+                requestPermission();
+            }
+        });
+
         return binding.getRoot();
+    }
+
+    private boolean checkPermissions() {
+        int cameraPermission = ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA);
+        return cameraPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+    }
+
+
+    private void captureImage() {
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePicture.resolveActivity(requireActivity().getPackageManager()) != null) {
+            someActivityResultLauncher.launch(takePicture);
+        }
+    }
+
+    private void detectText() {
+        if (imageBitmap == null) {
+            Toast.makeText(requireContext(), "Please capture an image first", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        InputImage image = InputImage.fromBitmap(imageBitmap, 0);
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        recognizer.process(image)
+                .addOnSuccessListener(text -> {
+                    StringBuilder result = new StringBuilder();
+                    for(Text.TextBlock block: text.getTextBlocks()){
+                        String blockText = block.getText();
+                        result.append(filterCountries(blockText));
+                    }
+                    binding.parkingWorkerHomeCarPlateEditTxt.setText(result);
+                })
+                .addOnFailureListener(e -> Toast.makeText(requireContext(), "Failed to detect text from image: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+    private String filterCountries(String text) {
+        String[] words = text.split("\\s+");
+        StringBuilder filteredText = new StringBuilder();
+        for (String word : words) {
+            if (!countriesToFilter.contains(word)) {
+                filteredText.append(word);
+            }
+        }
+        return filteredText.toString().trim();
     }
 
     private void navigateToParkingWorkerCheckoutFragment(String carPlateNumber) {
